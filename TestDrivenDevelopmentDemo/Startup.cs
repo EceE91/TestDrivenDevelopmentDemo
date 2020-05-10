eda9f09c94ab76c6aac4e7704d370b83c38a0cff
ece <eceercan91@hotmail.com>
2020-05-10 19:05:06 +0300

    bug fix

--- TestDrivenDevelopmentDemo/Startup.cs
+++ TestDrivenDevelopmentDemo/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using TestDrivenDevelopmentDemo.Api;
 
 namespace TestDrivenDevelopmentDemo
 {
@@ -24,6 +25,7 @@ namespace TestDrivenDevelopmentDemo
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddTransient<ICalculatorService, Calculator>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
